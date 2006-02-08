f2c74a8bda0aaa95a5a7888d00dd15e6d2336705
James MacDonald <jmacdon@med.umich.edu>
2006-02-08 21:32:16 +0000

    JWM: added an .onUnload() function to allow unloading of the shared library without quitting R, added copaFilter(), a function to pre-filter microarray data based on number of outlier samples, and added/modified all required man pages.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/copa@16146 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/copa.R
+++ R/copa.R
@@ -39,9 +39,130 @@ getans <- function(msg, allowed = c("y","n")){
   answer
 }
 
+## A function to create a matrix of permuted group assignments
 
-copa <- function(object, cutoff = 5, max.overlap = 0){
+perm.mat <- function(B, ids){
+	mat <- matrix(NA, nrow=B, ncol=length(ids))
+	for(i in 1:B){
+		mat[i,] <- sample(ids, length(ids))
+	}
+	invisible(return(mat))
+}
 
+
+copa <- function(object, cl, cutoff = 5, max.overlap = 0, norm.count = 0, pct = 0.95){
+  
+  mat <- copaFilter(object, cl, cutoff, norm.count, pct)
+  
+  ng <- dim(mat)[1]
+  ncomp <- choose(ng, 2)
+  if(dim(mat)[1] > 1000){
+    doit <-  getans(paste("\nYou are attempting to run copa on a large number of genes(",
+                          dim(mat)[1], ").\n Which will result in ", ncomp, " pairwise comparisons.\n",
+                          "This may take a long time and/or require lots of memory.\n",
+                          "An alternative would be to increase the value of pct higher than",
+                          pct, ".\n", "Do you want to continue?", sep=""))
+    
+    if(doit == "n"){
+      return(cat("copa aborted at user request\n"))
+    }
+  }
+  
+  ## Get gene pairs that have mutually exclusive outliers
+  outlier <- mat[,cl == 2] > cutoff 
+  outpairs <- crossprod(t(outlier))
+
+  ## Get row sums and pairwise row sums
+  r.sum <- rowSums(outlier)
+  pr.sums <- pSum(r.sum)
+
+  ## find pairs where outliers are mutually exclusive and row sums are 'large'
+  ## use lower.tri to remove diagonal and duplicated comparisons
+  outpairs <- outpairs[lower.tri(outpairs)]
+  pr.sums <- pr.sums[lower.tri(pr.sums)]
+  index <- outpairs <= max.overlap
+  ord <- order(pr.sums[index], decreasing = TRUE)
+  mat1 <- matrix(1:dim(mat)[1], nc = dim(mat)[1], nr = dim(mat)[1])
+  mat2 <- t(mat1)           
+  pr.idx <- cbind(mat1[lower.tri(mat1)], mat2[lower.tri(mat2)])
+  ord.prs <- pr.idx[index,][ord,]
+  return(structure(list(ord.prs = ord.prs, pr.sums = pr.sums[index][ord],
+                        mat = mat, cl = cl, cutoff = cutoff,
+                        max.overlap = max.overlap, norm.count = norm.count,
+                        pct = pct), class = "copa"))
+}
+
+plot.copa <- function(copa, idx, lib = NULL, sort = TRUE, col = NULL){
+
+  ## If affy chip, use available library
+  if(!is.null(lib))
+    require(lib, character.only = TRUE) || stop(paste("The package", lib, "is not installed!\n"))
+  
+  layout(matrix(1:2, nc=1))
+  ## convert ordered pair matrix into vector
+  if(is.matrix(copa$ord.pr[idx,]))
+    gn.idx <- as.vector(apply(copa$ord.prs[idx,], 1, c))
+  else
+    gn.idx <- copa$ord.prs[idx,]
+  grps <- unique(sort(copa$cl))
+  for(i in seq(along = gn.idx)){
+    if(!is.null(lib))
+      symb <- get(row.names(copa$mat)[gn.idx[i]],
+                  get(paste(lib, "SYMBOL", sep="")))
+    else
+      symb <- row.names(copa$mat)[gn.idx[i]]
+    if(sort){
+      tmp.lst <- vector("list", length(grps))
+      for(j in seq(along = grps)){
+        ord <- order(unlist(copa$mat[gn.idx[i], copa$cl == grps[j]]))
+        tmp.lst[[j]] <- copa$mat[gn.idx[i], copa$cl == grps[j]][ord]
+      }
+      barplot(unlist(tmp.lst), main = symb, col = rep(col, table(copa$cl)))
+    }else{
+      tmp.lst <- vector("list", length(grps))
+      for(j in seq(along = grps))
+        tmp.lst[[j]] <- copa$mat[gn.idx[i], copa$cl == grps[j]]
+        barplot(unlist(tmp.lst), main = symb, col = rep(col, table(copa$cl)))
+    }
+  }
+}
+
+
+
+copaPerm <- function(object, copa, outlier.num, B = 100, pval = FALSE){
+ 
+  perm <- perm.mat(B, copa$cl)
+  prmvals <- vector("list", B)
+  for(i in 1:B){
+    mat <- copaFilter(object, copa$cl, copa$cutoff, copa$norm.count, copa$pct)
+
+    ## Get gene pairs that have mutually exclusive outliers
+    outlier <-mat[,perm[i,] == 2] > copa$cutoff 
+    outpairs <- crossprod(t(outlier))
+
+    ## Get row sums and pairwise row sums
+    r.sum <- rowSums(outlier)
+    pr.sums <- pSum(r.sum)
+    
+    ## find pairs where outliers are mutually exclusive and row sums are 'large'
+    ## use lower.tri to remove diagonal and duplicated comparisons
+    outpairs <- outpairs[lower.tri(outpairs)]
+    pr.sums <- pr.sums[lower.tri(pr.sums)]
+    index <- outpairs <= copa$max.overlap
+    prmvals[[i]] <- pr.sums[index]
+  } 
+  out <- sapply(prmvals, function(x) sum(x >= outlier.num))
+  if(pval){
+    p.value <- sum(out <= outlier.num)/B
+    fdr <- mean(out)
+    return(list(out = out, p.value = p.value, fdr = fdr))
+  }else{
+    return(out)
+  }
+}
+
+copaFilter <- function(object, cl, cutoff, norm.count, pct){
+  
   mat <- NULL
   if(is(object, "exprSet")){
     mat <- exprs(object)
@@ -60,79 +181,23 @@ copa <- function(object, cutoff = 5, max.overlap = 0){
   }
   if(is.null(mat))
     mat <- as.matrix(object)
-  
-  ng <- dim(mat)[1]
-  ncomp <- ng * (ng - 1)/2
-  if(dim(mat)[1] > 1000){
-   doit <-  getans(paste("\nYou are attempting to run copa on a large number of genes(",
-                 dim(mat)[1], ").\n Which will result in ", ncomp, " pairwise comparisons.\n",
-                 "This may take a long time and/or require lots of memory.\n",
-                 "Do you want to continue?", sep=""))
-  
-   if(doit == "n"){
-     return(cat("copa aborted at user request\n"))
-   }
- }
-  
+
   ## Median center and scale using MAD
   med <- apply(mat, 1, median)
   MAD <- apply(mat, 1, mad)
   mat <- sweep(mat, 1, med, "-")
   mat <- sweep(mat, 1, MAD, "/")
 
-  ## Get gene pairs that have mutually exclusive outliers
-  outlier <- mat > cutoff
-  outpairs <- crossprod(t(outlier))
+  outlier <- mat[,cl == 2] > cutoff
+  badnorm <- mat[,cl == 1] > cutoff
 
-  ## Get row sums and pairwise row sums
-  r.sum <- rowSums(outlier)
-  pr.sums <- pSum(r.sum)
-
-  ## find pairs where outliers are mutually exclusive and row sums are 'large'
-  ## use lower.tri to remove diagonal and duplicated comparisons
-  outpairs <- outpairs[lower.tri(outpairs)]
-  pr.sums <- pr.sums[lower.tri(pr.sums)]
-  index <- outpairs <= max.overlap
-  ord <- order(pr.sums[index], decreasing = TRUE)
-  mat1 <- matrix(1:dim(mat)[1], nc = dim(mat)[1], nr = dim(mat)[1])
-  mat2 <- t(mat1)           
-  pr.idx <- cbind(mat1[lower.tri(mat1)], mat2[lower.tri(mat2)])
-  ord.prs <- pr.idx[index,][ord,]
-  return(structure(list(ord.prs = ord.prs, pr.sums = pr.sums[index][ord],
-                        mat = mat), class = "copa"))
-}
-
-plot.copa <- function(copa, idx, cl, lib = NULL, sort = TRUE, col = NULL){
-
-  ## If affy chip, use available library
-  if(!is.null(lib))
-    require(lib, character.only = TRUE) || stop(paste("The package", lib, "is not installed!\n"))
+  num.out <- rowSums(outlier)
+  num.bad <- rowSums(badnorm)
   
-  layout(matrix(1:2, nc=1))
-  ## convert matrix into vector ordered by row
-  if(is.matrix(copa$ord.pr[idx,]))
-    gn.idx <- as.vector(apply(copa$ord.prs[idx,], 1, c))
-  else
-    gn.idx <- copa$ord.prs[idx,]
-  if(is.factor(cl)) grps <- levels(cl) else grps <- unique(cl)
-  for(i in seq(along = gn.idx)){
-    if(!is.null(lib))
-      symb <- get(row.names(copa$mat)[gn.idx[i]],
-                  get(paste(lib, "SYMBOL", sep="")))
-    else
-      symb <- row.names(copa$mat)[gn.idx[i]]
-    if(sort){
-      tmp.lst <- vector("list", length(grps))
-      for(j in seq(along = grps)){
-        ord <- order(unlist(copa$mat[gn.idx[i], cl == grps[j]]))
-        tmp.lst[[j]] <- copa$mat[gn.idx[i], cl == grps[j]][ord]
-      }
-      barplot(unlist(tmp.lst), main = symb, col = rep(col, table(cl)))
-    }else{
-      tmp.lst <- vector("list", length(grps))
-      for(j in seq(along = grps))
-        tmp.lst[[j]] <- copa$mat[gn.idx[i], cl == grps[j]]
-        barplot(unlist(tmp.lst), main = symb, col = rep(col, table(cl)))
-    }
-  }
+  num.out <- num.out[num.bad <= norm.count]
+  mat <- mat[num.bad <= norm.count,]
+  index <- num.out >= quantile(num.out, probs = pct)
+  mat <- mat[index,]
+  mat
 }
+  
