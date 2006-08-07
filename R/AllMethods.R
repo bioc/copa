853960a0539f9642b8460b4170a46d806833f5e8
James MacDonald <jmacdon@med.umich.edu>
2006-08-07 14:24:52 +0000

    JWM:Forgot to commit new S4 methods for copa, which caused it not to build.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/copa@19056 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/AllMethods.R
@@ -0,0 +1,23 @@
+## S4 methods for the copaFilter function
+
+setGeneric("copaFilter", function(object, ...) standardGeneric("copaFilter"))
+
+setMethod("copaFilter", "matrix", function(object, cl, cutoff, norm.count, pct){
+  do.copaFilter(object, cl, cutoff, norm.count, pct)
+})
+
+setMethod("copaFilter", "data.frame", function(object, cl, cutoff, norm.count, pct){
+  object <- as.matrix(object)
+  do.copaFilter(object, cl, cutoff, norm.count, pct)
+})
+
+setMethod("copaFilter", "exprSet", function(object, cl, cutoff, norm.count, pct){
+  object <- exprs(object)
+  do.copaFilter(object, cl, cutoff, norm.count, pct)
+})
+
+setMethod("copaFilter", "ExpressionSet", function(object, cl, cutoff, norm.count, pct){
+  object <- exprs(object)
+  do.copaFilter(object, cl, cutoff, norm.count, pct)
+})
+
